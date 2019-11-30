33a1116f687882a19393b853c0954cc3aa3ddad2
Sorrowfulfloyd <57991464+Sorrowfulfloyd@users.noreply.github.com>
2019-11-30 17:22:10 +0300

    Update 3.4.cpp

--- Chapter 3/3.4/3.4.cpp
+++ Chapter 3/3.4/3.4.cpp
@@ -16,15 +16,28 @@ int main()
     int val2 = 0;
     cout << "Please enter two integers seperated by a space.\n";
     while(cin >> val1 >> val2){
-    if(val1 > val2)
+
+    if(val1 > val2) // Checking both values using basic if-else statements.
         cout << val1 << " is greater than " << val2 << '\n';
     else
-        cout << val2 << " is greater than " << val1 << '\n';    
+        cout << val2 << " is greater than " << val1 << '\n'; 
     if(val1 == val2)
         cout << val1 << " and " << val2 << " are equal.\n";
-    cout << "Sum: " << val1+val2 << '\n';
-    cout << "Difference: " << val1-val2 << '\n';   
-    cout << "Product: " << val1*val2 << '\n';
+
+
+    cout << "Sum: " << val1+val2 << '\n'; // Getting the sum.
+    cout << "Difference: " << val1-val2 << '\n'; // Difference.
+    cout << "Product: " << val1*val2 << '\n'; // Product.
+
+    if(val2 == 0) // Checking the second value, if it's zero we don't want our app to run into a error.
+        cout << "Can't divide " << val1 << " by zero.\n";
+    else // If not then proceed
+        cout << "Ratio of " << val1 << " and " << val2 << " is " << val1/val2 << '\n';
+
+
+    if(val2!=0 && val1 >= val2) // and making sure to include the remainder if there's any.
+        cout << "With a remainder of " << val1%val2;
     }
+    
     keep_window_open();
 }
