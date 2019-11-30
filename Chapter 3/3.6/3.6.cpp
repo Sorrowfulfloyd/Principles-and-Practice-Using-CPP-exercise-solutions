ff82d0ce307ca53b0cee2d2b453a0282902a534f
Sorrowfulfloyd <57991464+Sorrowfulfloyd@users.noreply.github.com>
2019-11-30 22:59:57 +0300

    update 3.6

---
+++ Chapter 3/3.6/3.6.cpp
@@ -0,0 +1,73 @@
+// Exercise 3.6 - Value Sorter
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+inline void keep_window_open() {  cout<<"Enter a key to exit.\n"; char ch; cin>>ch; } // Depending on your compiler you shall need this to keep the app open...
+
+
+int main() {
+    int val1 = 0;
+    int val2 = 0;
+    int val3 = 0;
+    int small = 0; 
+    int medium = 0;
+    int xlarge = 0;
+    cout << "Enter three numbers to get them sequenced from smallest to biggest.\n";
+    while(cin >> val1 >> val2 >> val3)
+    {
+        if(val1 <= val2 && val1 <= val3)
+        {
+            small = val1;
+        }
+        else if(val2 <= val3)
+        {
+            medium = val2;
+            xlarge = val3; 
+        }
+        else 
+        {
+            xlarge = val2;
+            medium = val3;
+        }
+        if(val2 <= val1 && val2 <= val3)
+        {
+            small = val2;
+        }
+        else if(val1 <= val3)
+        {
+            medium = val1;
+            xlarge = val3;
+        }
+        else
+        {
+            xlarge = val1;
+            medium = val3;
+        }
+        if(val3 <= val1 && val3 <= val2)
+        {
+            small = val3;
+        }
+        else if(val1 <= val2)
+        {
+            medium = val1;
+            xlarge = val2;
+        }
+        else
+        {
+            xlarge = val1;
+            medium = val2;
+        }
+        
+    cout << "The number you've entered from smallest to biggest are : " << small << " " << medium << " " << xlarge << '\n'; 
+
+    }
+
+
+
+    keep_window_open();
+}
